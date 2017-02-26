476612c72e2507999fed60de522d82c5ee410c57
kcirekcom <erick.f.mock@gmail.com>
2017-02-25 16:30:38 -0800

    added propTypes checking///styles

--- index.jsx
+++ index.jsx
@@ -3,13 +3,89 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-class HelloAndrew extends React.Component {
+class Timer extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      seconds: 0,
+      expired: false
+    };
+  }
+
+  componentDidMount() {
+    setInterval(() => {
+      this.setState({
+        seconds: this.state.seconds + 1
+      });
+    }, 1000);
+  }
+
+  shouldComponentUpdate(nextProps, nextState) {
+    console.log(nextState);
+    return true;
+  }
+
   render() {
-    return <div>Hello, Andrew</div>;
+    return (
+      <div>
+        {this.state.seconds}
+      </div>
+    );
+  }
+}
+
+const style = {
+  backgroundColor: '#CCC',
+  width: '100vw',
+
+};
+
+class UserCard extends React.Component {
+  render() {
+    return (
+      <div className='my-class' id='my-id' style={style}>
+        {this.props.name}
+
+        {this.props.email ? (
+          <span>{this.props.email}</span>
+        ) : null}
+      </div>
+    );
+  }
+}
+
+UserCard.propTypes = {
+  name: React.PropTypes.string.isRequired,
+  email: React.PropTypes.string
+};
+
+class App extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      user_name: 'Erick'
+    };
+  }
+
+  componentDidMount() {
+    setTimeout(() => {
+      this.setState({
+        user_name: 'Andrew'
+      });
+    }, 1000);
+  }
+
+  render() {
+    return (
+      <div>
+        <Timer/>
+        <UserCard name="erick" email="erick@email.com"/>
+      </div>
+    );
   }
 }
 
 ReactDOM.render(
-  <HelloAndrew/>,
+  <App/>,
   document.getElementById('first-render')
 );
